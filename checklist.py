0ae1c5cdb7e72a6933a30c4b40cdfb1af0a30f47
Andrew Alsing <111094607+Andrew32A@users.noreply.github.com>
2022-09-21 23:26:51 -0700

    Add files via upload
    Added main loop and a few more functions

--- checklist.py
+++ checklist.py
@@ -2,22 +2,21 @@
 # He can never wear the same color on any of his items. If he is wearing a purple shoe, he cannot wear a second purple shoe.
 # He must wear every color of the rainbow each day.
 
+# create list
 checklist = list()
 
+# define Functions
 def list_all_items():
     index = 0
     for list_item in checklist:
-        print (str(index) + list_item)
+        print("{} {}".format(index, list_item))
         index += 1
 
-
-# Define Functions
 def create(item):
     checklist.append(item)
 
 def read(index):
-    print(index)
-    print(index)
+    return checklist[index]
 
 def update(index, item):
     checklist[index] = item
@@ -25,28 +24,67 @@ def update(index, item):
 def destroy(index):
     checklist.pop(index)
 
-def test():
-    create("purple socks")
-    create("red shirt")
+# stretch challenge for later
+# def mark_completed(index):
+    
+def user_input(prompt):
+    user_input = input(prompt).lower()
+    return user_input
 
-    read(0)
-    read(1)
+def select(function_code):
+    if function_code == "c":
+        input_item = user_input("Input item: ")
+        create(input_item)
 
-    update(0, "purple socks")
+    elif function_code == "r":
+        item_index = user_input("Index Number?: ")
+        # remember that item_index must actually exist or our program will crash.
+        read(item_index)
 
-    destroy(1)
+    elif function_code == "p":
+        list_all_items()
 
-    read(0)
+    elif function_code == "q":
+        return False
 
-    list_all_items()
+    else:
+        print("Unknown Option")
 
-test()
+    # keeps loop running
+    return True
+
+# main loop
+running = True
+while running:
+    selection = user_input("Press C to add to list, R to Read from list, P to display list, and Q to quit: ")
+    running = select(selection)
 
 
 
 
-# random stuff
 
+
+
+# testing area
+# def test():
+#     create("purple socks")
+#     create("red shirt")
+
+#     read(0)
+#     read(1)
+
+#     update(0, "purple socks")
+
+#     destroy(1)
+
+#     read(0)
+
+#     list_all_items()
+
+# test()
+
+
+# random stuff i wanted to try
 # colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
 # clothes = ["hat", "sunglasses", "shirt", "pants", "underwear", "socks", "shoes"]
 # days = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"]
